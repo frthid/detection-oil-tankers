c971572dfe1144a0be4db34e5cd7eeb759532120
frthid <zvezdochets02@gmail.com>
2023-07-17 23:33:42 +0300

    добавил тренировочную часть питоновским модулем

--- src/train_modal/train.py
+++ src/train_modal/train.py
@@ -1,5 +1,9 @@
 from ultralytics import YOLO
+from IPython import display
 
 model = YOLO('src/model/yolov8m.pt')
+model.train(data='src/train_modal/data.yaml', epochs=30, imgsz=512, plots=True, device=0)
 
-model.train(data='src/train_modal/config.yaml', epochs=5, imgsz=640)
+Image(filename='runs/detect/train/confusion_matrix.png', width=800)
+Image(filename='runs/detect/train/results.png', width=800)
+Image(filename='runs/detect/train/val_batch0_pred.jpg', width=800)
